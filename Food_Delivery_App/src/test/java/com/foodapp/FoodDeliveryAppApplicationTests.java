e76e51232f5c77846b0145b6b187ec6fadc308d6
Aamir Sohail <aamirsohail763@gmail.com>
2022-09-30 13:06:35 +0530

    project added

---
+++ Food_Delivery_App/src/test/java/com/foodapp/FoodDeliveryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
